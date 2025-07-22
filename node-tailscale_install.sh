f584b33404316708b041fb3f3d35f7b9897bc88d
Haziq Zabridin <hzabridin@gmail.com>
2025-07-22 17:35:20 +0800

    Update node-tailscale_install.sh

--- node-tailscale_install.sh
+++ node-tailscale_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# *****************************************************
+echo "*****************************************************
 #   N   N  OOO   DDDD   EEEEE   RRRR    EEEEE   DDDD
 #   NN  N O   O  D   D  E       R   R   E       D   D
 #   N N N O   O  D   D  EEEE    RRRR    EEEE    D   D
@@ -9,40 +9,40 @@
 #                                                    
 #               Script created by: haziq
 #       Automating the Node-RED installation process!   
-# *****************************************************
+# *****************************************************"
 
-# Update system
+
 echo "Updating system..."
 sudo apt update -y
 sudo apt upgrade -y
 
-# Install necessary dependencies
+
 echo "Installing build-essential and python3..."
 sudo apt install -y build-essential python3
 
-# Install NVM (Node Version Manager)
+
 echo "Installing NVM..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-# Source NVM for the current session
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-# Install Node.js using NVM (Node.js version 18)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+
+
 echo "Installing Node.js version 18..."
 nvm install 18
 nvm use 18
 nvm alias default 18
 
-# Install Node-RED
+
 echo "Installing Node-RED..."
 npm install -g --unsafe-perm node-red
 
-# Install necessary Node-RED nodes (e.g., node-red-node-serialport)
+
 echo "Installing Node-RED nodes..."
 npm install --no-audit --no-update-notifier --no-fund --save --save-prefix=~ --omit=dev --engine-strict node-red-node-serialport@2.0.3
 
-# Create a systemd service for Node-RED
+
 echo "Setting up Node-RED systemd service..."
 
 cat <<EOL | sudo tee /etc/systemd/system/nodered.service
@@ -69,19 +69,18 @@ SyslogIdentifier=node-red
 WantedBy=multi-user.target
 EOL
 
-# Set permissions and reload systemd
+
 sudo chown -R admin:admin /home/admin/.nvm
 sudo systemctl daemon-reload
 
-# Enable Node-RED to start on boot
 echo "Enabling Node-RED to start on boot..."
 sudo systemctl enable nodered
 
-# Start Node-RED service
+
 echo "Starting Node-RED..."
 sudo systemctl start nodered
 
-# Check the status of Node-RED
+
 echo "Checking Node-RED status..."
 sudo systemctl status nodered
 
@@ -89,7 +88,6 @@ echo "--------------------NODE-RED INSTALLED!-----------------------------------
 echo "............................................................................."
 echo "---------------------INSTALLING TAILSCALE------------------------------------"
 
-# Install Tailscale using a separate script
 curl -sSL https://raw.githubusercontent.com/iyon09/Bivocom-Node-RED-Tailscale-/main/DanLab_BV2.sh | bash
 
 echo "--------------------TAILSCALE INSTALLED!------------------------------------"
